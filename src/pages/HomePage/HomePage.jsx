9da312d732b4c5dc15c126b0645929269ed3f969
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-04-28 21:29:06 +0100

    Create HomePage.jsx

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+const HomePage = () => {
+  return (
+    <>
+      <p>
+        Hello :D My name is Daniel. This is my phonebook site. It just works...
+        Enjoy:)
+      </p>
+      <div>
+        <img
+          src="/src/images/raccoon-ayasan.gif"
+          alt="racoon riding a scooter"
+        />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
